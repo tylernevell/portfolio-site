8066cface534b3f5044f3b12193619230c542132
Tyler Nevell <tylernevell@gmail.com>
2021-04-19 16:04:12 -0700

    v1.0.0

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
+// const mongoose = require('mongoose');
 
-const items = require('./routes/api/items');
+// const items = require('./routes/api/items');
 
 const app = express();
 
@@ -12,15 +13,26 @@ const app = express();
 app.use(bodyParser.json());
 
 // DB Config & connect to mongoose
-const db = require('./config/keys').mongoURI;
+// const db = require('./config/keys').mongoURI;
+//
+// mongoose
+//     .connect(db, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+//     .then(() => console.log('MongoDB Connected.'))
+//     .catch(err => console.log(err));
 
-mongoose
-    .connect(db, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
-    .then(() => console.log('MongoDB Connected.'))
-    .catch(err => console.log(err));
+// // Use Routes
+// app.use('/api/items', items);
 
-// Use Routes
-app.use('/api/items', items);
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set Static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+
+}
 
 const port = process.env.PORT || 5000;
 
